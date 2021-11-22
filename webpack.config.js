f76cef34c6d2fe9360b0da56ff7b6dc84b62cbdf
igor.kuzminov <igor.kuzminov@jetbrains.com>
2021-11-22 11:49:55 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        hashDigestLength: 1
+    },
+};
